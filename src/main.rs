9fb771d0eadb81f3ab1ff3e4d40a68978383264e
Tijmen <tijmenspijk@gmail.com>
2022-06-15 19:12:03 +0200

    initial file setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
     println!("Hello, world!");
 }
+
+/* TODO */
+// reading an image
+// converting image to 2d array
+// converting 2d array to network of nodes
+// creating the node class
+// solving the network => solving the maze
+// implementing path finding algorithms
